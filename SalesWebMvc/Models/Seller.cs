f0861566429ef2464341622f5e5a1934d2499074
Diego de Andrade <diego.de.andrade88@gmail.com>
2020-08-03 10:55:55 -0300

    Inclusão de Formatações e Validações

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} deve ser informado")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter o tamanho entre {1} e {2}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} deve ser informado")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido")]
+        [DataType(DataType.EmailAddress)]   
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} deve ser informado")]
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} deve ser informado")]
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
